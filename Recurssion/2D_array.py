635c1cb2ee28988c96bcb41c046a1e89f749dfa4
agowdagr <abhiishekgowda@gmail.com>
2021-07-29 18:52:09 -0700

    2D array in python using numpy

---
+++ Recurssion/2D_array.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr1 = np.array([[4,5,6,7,1],[2,9,0,1,3],[6,3,0,3,4]])
+print(arr1)
+#time complexity is O(1)
+#space complexity is O(mn)
+
+#to add a new row or column in the begining
+new_arr= np.insert(arr1,0,[[1,2,3,4,5]],axis=0)
+print(new_arr)
+
+#to add new row or coulmn at the end
+
+new_arr1= np.append(arr1,[[5,6,7,8,9]],axis=0)
+print(new_arr1)
