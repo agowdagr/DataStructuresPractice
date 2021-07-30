bf56254441d41b4b37246f65409a01111a59e1a8
agowdagr <abhiishekgowda@gmail.com>
2021-07-30 01:15:41 -0700

    Max consecutive sum in array(Sliding Window)

---
+++ Sliding window/Max_consSum_slidingW.py
@@ -0,0 +1,15 @@
+#finding the maximum sum of k consective elements in a given array
+#solving this problem using sliding window
+
+def max_sum(arr,k):
+    n=len(arr)
+    max_count=sum([arr[i] for i in range(0,k)])
+    cur_count= max_count
+    for j in range (0,n-k):
+        cur_count= cur_count - arr[j] + arr[j+k]
+        max_count= max(max_count,cur_count )
+    return max_count
+
+if __name__=='__main__':
+    arr=[80,-50,100,90,200]
+    print(max_sum(arr,2))
