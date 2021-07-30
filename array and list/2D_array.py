aac5771ba7f7f1ebe7d110dbc9067ebcc3479597
agowdagr <abhiishekgowda@gmail.com>
2021-07-29 23:12:06 -0700

    2D array in python using numpy

--- array and list/2D_array.py
+++ array and list/2D_array.py
@@ -7,9 +7,13 @@ print(arr1)
 
 #to add a new row or column in the begining
 new_arr= np.insert(arr1,0,[[1,2,3,4,5]],axis=0)
-print(new_arr)
+#print(new_arr)
 
 #to add new row or coulmn at the end
 
 new_arr1= np.append(arr1,[[5,6,7,8,9]],axis=0)
-print(new_arr1)
+#print(new_arr1)
+ #for deleting 1st row or column
+
+new_arr2= np.delete(arr1,0,axis=1)
+print(new_arr2)
